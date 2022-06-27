bcf0b627a7a65b8ec96db26064a8378404533708
EdinsonFlorez <edinson_florez.1@hotmail.com>
2022-06-27 13:55:35 -0500

    version 2.0

--- src/ejerciciopdf211/EjercicioPDF211.java
+++ src/ejerciciopdf211/EjercicioPDF211.java
@@ -20,18 +20,31 @@ public class EjercicioPDF211 {
             
             switch(meses){
                 case 1: System.out.println("El mes ingresado es enero y tiene 31 dias.");
+                break;
                 case 2: System.out.println("El mes ingresado es febrero y tiene 28 dias.");
+                break;
                 case 3: System.out.println("El mes ingresado es marzo y tiene 31 dias.");
+                break;
                 case 4: System.out.println("El mes ingresado es abril y tiene 30 dias.");
+                break;
                 case 5: System.out.println("El mes ingresado es mayo y tiene 31 dias.");
+                break;
                 case 6: System.out.println("El mes ingresado es junio y tiene 30 dias.");
+                break;
                 case 7: System.out.println("El mes ingresado es julio y tiene 30 dias.");
+                break;
                 case 8: System.out.println("El mes ingresado es agosto y tiene 31 dias.");
+                break;
                 case 9: System.out.println("El mes ingresado es septiembre y tiene 30 dias.");
+                break;
                 case 10: System.out.println("El mes ingresado es octubre y tiene 31 dias.");
+                break;
                 case 11: System.out.println("El mes ingresado es noviembre y tiene 30 dias.");
+                break;
                 case 12: System.out.println("El mes ingresado es diciembre y tiene 31 dias.");
+                break;
                 }
             }
+        System.out.println("Codigo finalizado.");
             }
         }
